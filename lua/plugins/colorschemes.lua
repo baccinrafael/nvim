75ae121b6ce87f2a949394f4eae643f75e854cf2
Rafael Baccin <akabacc@gmail.com>
2026-08-04 09:07:09 -0300

    colorschemes and p5.js

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,6 @@
+return {
+  { "EdenEast/nightfox.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "sainnhe/everforest" },
+  { "scottmckendry/cyberdream.nvim" },
+}
---
+++ lua/plugins/p5js.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "prjctimg/p5.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("p5").setup({})
+    end,
+  },
+}
